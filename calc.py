801b23beaf537808a11605ce207c6fc3684c2f80
EduGranado <almeida.granado@gmail.com>
2021-02-28 00:37:48 -0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+num1 = int(input("Digite o primeiro número: "))
+op = input("Digite o operador: ")
+num2 = int(input("digite o segunto valor: "))
+#adição +
+if op == "+":
+    result = num1 + num2
+#subtração -
+elif op == "-":
+    result = num1 + num2
+#subtração /
+elif op == "/":
+    result = num1 / num2
+#subtração *
+elif op == "*":
+    result = num1 + num2
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
